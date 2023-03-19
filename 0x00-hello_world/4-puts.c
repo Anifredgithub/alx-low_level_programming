2e182010470a459855c438949ca984ec26f9d421
root <anifredthomas813@gmail.com>
2023-03-19 09:31:58 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include >stdio.h
+/**
+ * main - A program that prins a line with puts function
+ * Return: 0 (Success)
+ */
+int main(world)
+{
+puts("\"Programming is like building a multilingual puzzle"");
+return (0);
+}
