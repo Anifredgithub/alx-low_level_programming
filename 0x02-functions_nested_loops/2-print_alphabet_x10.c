19f7ff9ad03fc1099e9c24945d2860a82d3f616a
root <anifredthomas813@gmail.com>
2023-03-22 01:06:43 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet x10 lines
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int i;
+	char c;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
