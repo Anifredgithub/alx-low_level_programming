e219627f2c5ec4c200d0833d09cddf87e69fa7dc
root <anifredthomas813@gmail.com>
2023-03-30 16:02:38 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,9 @@ char *cap_string(char *str)
 	while (str[index])
 	{
 	while (!(str[index] >= 'a' && str[index] <= 'z'))
+
 	index++;
+
 	if (str[index - 1] == ' ' ||
 	str[index - 1] == '\t' ||
 	str[index - 1] == '\n' ||
@@ -28,7 +30,8 @@ char *cap_string(char *str)
 	str[index - 1] == '{' ||
 	str[index - 1] == '}' ||
 	index == 0)
-	str[index - 1] -= 32;
+
+	str[index] -= 32;
 	index++;
 	}
 	return (str);
