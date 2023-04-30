aa2c65fc84bcf227e6a61b3159ad8358938cfd88
root <anifredthomas813@gmail.com>
2023-04-30 08:00:51 -0700

    Sum list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,13 +2,13 @@
 
 /**
  *sum_listint - calculates the sum of all the data (n) in a linked list
- * @head: head note in a linked list
+ * @head: head node in a linked list
  * Return: the sum of all the data, or 0 if the list is empty
  */
 
 int sum_listint(listint_t *head)
 {
-	int sum = 0
+	int sum = 0;
 	listint_t *tmp = head;
 
 	while (tmp)
